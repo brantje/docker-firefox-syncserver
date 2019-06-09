1175093c9bd8d9bab0b56208b41d7c94f308d668
Sander <brantje@gmail.com>
2019-06-09 12:46:48 +0200

    Add logger

--- run.sh
+++ run.sh
@@ -22,6 +22,32 @@ use = egg:syncserver
 public_url = ${URL}
 secret = ${SECRET}
 force_wsgi_environ = ${FORCE_WSGI}
+[loggers]
+keys = root, gunicorn
+
+[handlers]
+keys = console
+
+[formatters]
+keys = generic
+
+[logger_root]
+level = INFO
+handlers = console
+
+[logger_gunicorn]
+level = WARN
+handlers = console
+qualname = gunicorn
+
+[handler_console]
+class = StreamHandler
+args = (sys.stdout,)
+formatter = generic
+
+[formatter_generic]
+class = mozsvc.util.JsonLogFormatter
+
 " > /sync/syncserver.ini
 
 chown -R "${UID}:${GID}" .
